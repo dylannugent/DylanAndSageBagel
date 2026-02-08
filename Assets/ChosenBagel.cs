a8cea4a90791fbe9c0c208201bd59f9249bd5fc1
Dylan Nugent <dylannugent26@gmail.com>
2026-02-08 00:11:43 -0500

    big update

---
+++ Assets/ChosenBagel.cs
@@ -0,0 +1,81 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChosenBagel : MonoBehaviour
+{
+
+    public GameObject thisBagel;
+    public Vector3[] spawnPosition;
+    private static bool[] spawnedHere = new bool[4];
+
+
+    void Start()
+    {
+       /* spawnPosition = new Vector3[]
+        {
+            new Vector3(-1,2,0),
+            new Vector3(1,2,0),
+            new Vector3(-1,3,0),
+            new Vector3(1,3,0)
+
+        }; */
+      
+      }
+
+
+    private Vector3 bestSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // 1. Get the current mouse reference from the New Input System
+        Mouse mouse = Mouse.current;
+
+        // 2. Check if the left button was JUST pressed
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePos = GetMouseWorldPos(mouse.position.ReadValue());
+
+            // Check if we clicked the knife's collider
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+
+                int i;
+
+                for (i = 0; i < 4; i++)
+                {
+
+                    if (spawnedHere[i] == false)
+                    {
+                        bestSpawn = spawnPosition[i];
+                        bestSpawn = spawnPosition[i];
+                        spawnedHere[i] = true;
+
+                        Instantiate(thisBagel, bestSpawn, Quaternion.identity);
+
+                        break;
+                    }
+
+
+                }
+
+            }
+        }
+
+
+    }
+
+    private Vector3 GetMouseWorldPos(Vector2 screenPos)
+    {
+        // We use 10 for Z because that's usually the distance from Camera to 0
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 10f));
+        worldPos.z = 0;
+        return worldPos;
+    }
+}
+
+
